04bd7aea664d85bd50775793ddfa6ba1f93a86b0
Oliver Haas <ohaas@e1plus.de>
2025-03-02 20:06:04 +0100

    fix: update outputs to use google_cloud_run_v2_job for consistency

--- modules/cloud-run-job/output.tf
+++ modules/cloud-run-job/output.tf
@@ -1,17 +1,12 @@
-# Return service URL
-output "url" {
-  value = google_cloud_run_service.this.status[0].url
-}
-
 output "location" {
-  value = google_cloud_run_service.this.location
+  value = google_cloud_run_v2_job.this.location
 }
 
 # Return Service name
 output "name" {
-  value = google_cloud_run_service.this.name
+  value = google_cloud_run_v2_job.this.name
 }
 
 output "service_account" {
-  value = google_cloud_run_service.this.template[0].spec[0].service_account_name
+  value = google_cloud_run_v2_job.this.template[0].template[0].service_account_name
 }
