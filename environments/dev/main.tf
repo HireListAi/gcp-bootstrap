56b1558be5648a9fabd8f4481db20a471a61753b
Oliver Haas <ohaas@e1plus.de>
2025-03-02 20:00:29 +0100

    fix: update billing account and org ID for dev and prod environments

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,8 +11,8 @@ provider "google-beta" {
 module "project" {
   source = "../../modules/project"
 
-  project_id = "dev-source-atlas-443719"
-  project_name = "dev-source-atlas"
-  billing_account = "0165B4-557293-D88C4C"
-  org_id = "253981344865"
+  project_id      = "dev-source-atlas-443719"
+  project_name    = "dev-source-atlas"
+  billing_account = "017DBF-6EA665-FE7990"
+  org_id          = "366827031981"
 }
